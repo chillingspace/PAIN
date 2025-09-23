879e49d04672ebf635e606bf3945d8389b4bed9b
Rubberduckduck <bryanlim805@gmail.com>
2025-09-23 10:40:38 +0800

    Descriptor files
    Resources Pipelines
    
    With setup of relavant files

---
+++ PAINEngine/src/PAINEngine/CoreSystems/Assets/sAssetCompiler.h
@@ -0,0 +1,31 @@
+﻿/*****************************************************************//**
+ * \file   sAssetCompiler.h
+ * \brief  Declaration of asset compiler service
+ *
+ * \author Bryan Lim, 2301214, bryanlicheng.l@digipen.edu (100%)
+ * \co-author
+ * \date   September 2025
+ * All content � 2025 DigiPen Institute of Technology Singapore, all rights reserved.
+ *********************************************************************/
+
+#pragma once
+
+#ifndef S_ASSET_COMPILER_H
+#define S_ASSET_COMPILER_H
+
+namespace PAIN {
+	namespace ASSET_COMPILER {
+		class Service
+		{
+		public:
+			Service();
+			~Service();
+
+		private:
+
+		};
+	}
+}
+
+
+#endif
---
+++ PAINEngine/src/PAINEngine/CoreSystems/Assets/sAssets.h
@@ -0,0 +1,9 @@
+﻿/*****************************************************************//**
+ * \file   sAssets.h
+ * \brief  Declaration of physics system states
+ *
+ * \author Bryan Lim, 2301214, bryanlicheng.l@digipen.edu (100%)
+ * \co-author
+ * \date   September 2025
+ * All content � 2025 DigiPen Institute of Technology Singapore, all rights reserved.
+ *********************************************************************/
