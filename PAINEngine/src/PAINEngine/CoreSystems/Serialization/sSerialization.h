7e360509130f4d2fc7aef573a9e62c0ed75aeae7
bsohzj <144084859+bsohzj@users.noreply.github.com>
2025-09-27 20:29:54 +0800

    Added serialization files

---
+++ PAINEngine/src/PAINEngine/CoreSystems/Serialization/sSerialization.h
@@ -0,0 +1,17 @@
+/*****************************************************************//**
+ * \file   sSerialization.h
+ * \brief  Declaration of serialization service
+ *
+ * \author Bryan Soh, 2301238, z.soh@digipen.edu (100%)
+ * \co-author
+ * \date   September 2025
+ * All content  2025 DigiPen Institute of Technology Singapore, all rights reserved.
+ *********************************************************************/
+#pragma once
+#ifndef SERIALIZATION_SERVICE_H
+#define SERIALIZATION_SERVICE_H
+
+#include "pch.h"
+#include "Applications/AppSystem.h"
+
+#endif
