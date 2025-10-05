97514c0a18c687d570978f28a01505e2fca9ba8c
MintKK <30825268+MintKK@users.noreply.github.com>
2025-10-05 17:41:22 +0800

    m_Objects no longer used, audio system still needs to be updated for ECS and services

--- PAINEngine/src/PAINEngine/CoreSystems/Scene/Scene.h
+++ PAINEngine/src/PAINEngine/CoreSystems/Scene/Scene.h
@@ -4,13 +4,9 @@
 #include "CoreSystems/Renderer/Mesh.h"
 #include "CoreSystems/Audio/Audio.h"
 #include "Camera.h"
+#include "ECS/ECSTypes.h" // Added for Entity::Type
 
 namespace PAIN {
-
-	struct SceneObject {
-		Mesh* mesh;
-		glm::mat4 transform;  // model matrix
-	};
 
 	class Scene : public AppSystem {
 	public:
@@ -23,37 +19,26 @@ namespace PAIN {
 		void onUpdate(AppTiming timing) override;
 		void onEvent([[maybe_unused]] Event::Event& e) override;
 
-		std::shared_ptr<Mesh> AddObject(std::shared_ptr<Mesh> mesh, glm::vec3 pos, glm::quat quat, glm::vec3 scale);
-		void DeleteObject(int index);
+		// Modified to return the created entity's ID
+		ECS::Entity::Type AddObject(std::shared_ptr<Mesh> mesh, glm::vec3 pos, glm::quat quat, glm::vec3 scale);
 
-		const std::vector<SceneObject>& GetObjects() const;
 		Camera* GetActiveCamera();
-		std::vector<Mesh*> m_Meshes;
-		//Entity CreateEntity(const std::string name);
-		//void DestroyEntity(Entity entity);
 
 	private:
-
-		std::vector<SceneObject> m_Objects;
 		std::unique_ptr<Camera> camera;
 
-		//std::vector<Camera*> cameras; // multiple cams impl
-		
-		
-		//std::unique_ptr<ECS::Controller> ecs;
-
 		// --- Audio Demo State Variables ---
-		int audioSourceObjectIndex = -1; // Index of the object that will be our audio source
-		Audio::AudioChannelId audioSourceChannel; // Channel ID for the looping sound
+		ECS::Entity::Type audioSourceEntity = ECS::Entity::INVALID; // Changed to store the entity ID
+		Audio::AudioChannelId audioSourceChannel;
 
 		// Path animation variables
 		float demoTime = 0.0f;
 		int currentPathSegment = 0;
-		float segmentDuration = 4.0f; // Time in seconds to travel one segment
+		float segmentDuration = 4.0f;
 		std::vector<glm::vec3> pathCorners;
 
 		// Footstep variables
 		float footstepTimer = 0.0f;
-		const float footstepInterval = 0.4f; // Play a footstep every 0.4 seconds
+		const float footstepInterval = 0.4f;
 	};
 }
