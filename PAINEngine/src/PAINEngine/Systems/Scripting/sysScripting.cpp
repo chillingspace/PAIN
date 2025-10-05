04779c75baf4c0a3e7802451feb43e622891eb42
nnntdk <lee.n@digipen.edu>
2025-10-05 16:20:57 +0800

    updated docu

--- PAINEngine/src/PAINEngine/Systems/Scripting/sysScripting.cpp
+++ PAINEngine/src/PAINEngine/Systems/Scripting/sysScripting.cpp
@@ -2,7 +2,7 @@
  * \file   sysScripting.cpp
  * \brief  Definition of scripting system states
  *
- * \author [Your Name], [ID], [email] (100%)
+ * \author Nicole Esther Lee, 2301544, lee.n@digipen.edu (100%)
  * \co-author
  * \date   October 2025
  * All content © 2025 DigiPen Institute of Technology Singapore, all rights reserved.
