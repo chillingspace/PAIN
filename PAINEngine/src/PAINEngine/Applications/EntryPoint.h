1ccaee5ad54f622befd3ce55b76420f3f7aa79d4
Rubberduckduck <bryanlim805@gmail.com>
2025-11-03 16:12:48 +0800

    Testing CI to check if it detects mem leaks

--- PAINEngine/src/PAINEngine/Applications/EntryPoint.h
+++ PAINEngine/src/PAINEngine/Applications/EntryPoint.h
@@ -12,6 +12,18 @@
 				_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 		#endif
 
+		
+		// For CI to run properly, to check for leaks and crashes
+		if (std::getenv("CI_TEST") != nullptr) {
+			std::thread([] {
+				std::this_thread::sleep_for(std::chrono::seconds(10));
+				std::exit(0);
+				}).detach();
+		}
+		
+		// Purposely added mem leak here to test in CI
+		int* leak = new int[1];
+
 		auto game = PAIN::CreateApplication();
 		game->Init();
 		game->Run();
