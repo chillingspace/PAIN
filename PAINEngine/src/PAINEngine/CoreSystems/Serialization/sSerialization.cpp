4382d05a6f46fa8c541591a7ac54025e90a2f4b5
Rubberduckduck <bryanlim805@gmail.com>
2025-11-03 22:30:47 +0800

    Update sSerialization.cpp

--- PAINEngine/src/PAINEngine/CoreSystems/Serialization/sSerialization.cpp
+++ PAINEngine/src/PAINEngine/CoreSystems/Serialization/sSerialization.cpp
@@ -112,7 +112,7 @@ namespace PAIN {
 
             std::string contents;
             contents.resize(stream->size());
-            stream->read(contents.data(), contents.size());\
+            stream->read(contents.data(), contents.size());
 
             // Validate JSON syntax before parsing
             if (!nlohmann::json::accept(contents)) {
