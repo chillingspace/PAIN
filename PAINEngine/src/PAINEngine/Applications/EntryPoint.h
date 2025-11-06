0f40c1021dc6de8ecd563d3a2b71c2eb6f0bbd31
aynarafaelle <ayna.rafaelle@gmail.com>
2025-11-07 02:14:12 +0800

    mem leak part 3

--- PAINEngine/src/PAINEngine/Applications/EntryPoint.h
+++ PAINEngine/src/PAINEngine/Applications/EntryPoint.h
@@ -9,7 +9,7 @@
 
 		//// Enable run-time memory check for debug builds.
 		#if defined(DEBUG) | defined(_DEBUG)
-		_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+		_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF);
 		#endif
 
 		{
