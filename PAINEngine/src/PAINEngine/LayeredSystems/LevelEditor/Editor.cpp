4dc63cd913c1ed9d1ca66b703d5556d147c34aa2
nnntdk <lee.n@digipen.edu>
2025-11-26 23:19:45 +0800

    Animation panel
    browse animation entities, select animations, control pause/play/stop/reset, adjust speed, loop, timeline and timing

--- PAINEngine/src/PAINEngine/LayeredSystems/LevelEditor/Editor.cpp
+++ PAINEngine/src/PAINEngine/LayeredSystems/LevelEditor/Editor.cpp
@@ -13,6 +13,7 @@
 #include "Panels/ViewportPanel.h"
 #include "Panels/EntityPanel.h"
 #include "Panels/DebugPanel.h"
+#include "Panels/AnimationPanel.h"
 
 #include "CoreSystems/Renderer/sRenderer.h"
 #include "CoreSystems/Serialization/sSerialization.h"
@@ -60,12 +61,17 @@ namespace PAIN {
             registerPanel(std::make_shared<Panel::Tools>());
             registerPanel(std::make_shared<Panel::AudioPanel>());
             registerPanel(std::make_shared<Panel::ScenesPanel>());
-            registerPanel(std::make_shared<Panel::ComponentsPanel>());
+
+            auto components_panel = std::make_shared<Panel::ComponentsPanel>();
+            registerPanel(components_panel);
 
             // Create ViewportPanel and link it to EntityPanel
             auto viewport_panel = std::make_shared<Panel::ViewportPanel>();
             viewport_panel->setEntityPanel(entity_panel);  // LINK THEM TOGETHER
             registerPanel(viewport_panel);
+
+            auto animation_panel = std::make_shared<Panel::AnimationPanel>();
+            registerPanel(animation_panel);
 
             registerPanel(std::make_shared<Panel::DebugPanel>());
 
