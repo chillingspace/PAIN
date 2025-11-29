a175c01168f84ebeeb45c4bf6381a884d2cd5566
hoshuhng <zendenho@gmail.com>
2025-11-29 18:32:15 +0800

    UI Update

--- Common/AssetPipeline/src/AssetCompiler.cpp
+++ Common/AssetPipeline/src/AssetCompiler.cpp
@@ -521,7 +521,7 @@ namespace PAIN {
                 }
 
                 //Compress asset
-                compression_success = CuttlefishCompressor(raw_pixels, width, height, 4,
+                compression_success = CuttlefishCompressor(raw_pixels, width, height, channels,
                     asset_info.shipped_path.string(),
                     compression_format, desc_file.import_settings);
 
