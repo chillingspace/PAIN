d58f9edce38bbe34d089346cfb003aafd1fc07b3
MintKK <30825268+MintKK@users.noreply.github.com>
2026-01-20 09:19:32 +0800

    Revert "Testing custom compound collider implementation"
    This reverts commit 02c86605957c8468e1a73973d34341202c19bce0.

--- PAINEngine/src/PAINEngine/CoreSystems/Assets/Types/Prefab.h
+++ PAINEngine/src/PAINEngine/CoreSystems/Assets/Types/Prefab.h
@@ -6,25 +6,21 @@
 #include "AssetTypes.h"
 
 namespace PAIN {
-namespace Prefab {
+	namespace Prefab {
 
-// Prefab asset class
-struct PrefabAsset : Assets::IAsset {
-  std::string prefabName;
-  Assets::GUID rootEntityGUID;
-  std::vector<nlohmann::json> entities;
+        //Prefab asset class
+        struct PrefabAsset : Assets::IAsset {
+            std::string prefabName;
+            Assets::GUID rootEntityGUID;
+            std::vector<nlohmann::json> entities;
 
-  // Custom collider shapes for this prefab (optional)
-  // When prefab is instantiated, these become the CompoundCollider component
-  std::vector<ColliderShape> defaultColliderShapes;
+            PrefabAsset() = default;
 
-  PrefabAsset() = default;
-
-  PrefabAsset(const std::string &name, const Assets::GUID &root,
-              std::vector<nlohmann::json> &&entities)
-      : prefabName(name), rootEntityGUID(root), entities{entities} {}
-};
-} // namespace Prefab
-} // namespace PAIN
+            PrefabAsset(const std::string& name, const Assets::GUID& root, std::vector<nlohmann::json>&& entities)
+                : prefabName(name), rootEntityGUID(root), entities{ entities } {
+            }
+        };
+	}
+}
 
 #endif
