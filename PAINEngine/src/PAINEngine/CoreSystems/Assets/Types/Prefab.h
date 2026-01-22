54d2ad6488d6356b2ca034c0a20a966ac08e979f
MintKK <30825268+MintKK@users.noreply.github.com>
2026-01-22 11:18:36 +0800

    Testing reformat changes

--- PAINEngine/src/PAINEngine/CoreSystems/Assets/Types/Prefab.h
+++ PAINEngine/src/PAINEngine/CoreSystems/Assets/Types/Prefab.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #ifndef ASSETS_PREFAB_HPP
 #define ASSETS_PREFAB_HPP
@@ -11,18 +11,18 @@ namespace Prefab {
 
 // Prefab asset class
 struct PrefabAsset : Assets::IAsset {
-  std::string prefabName;
-  Assets::GUID rootEntityGUID;
-  std::vector<nlohmann::json> entities;
+	std::string prefabName;
+	Assets::GUID rootEntityGUID;
+	std::vector<nlohmann::json> entities;
 
-  // Default compound collider shapes for this prefab
-  std::vector<ColliderShape> defaultColliderShapes;
+	// Default compound collider shapes for this prefab
+	std::vector<ColliderShape> defaultColliderShapes;
 
-  PrefabAsset() = default;
+	PrefabAsset() = default;
 
-  PrefabAsset(const std::string &name, const Assets::GUID &root,
-              std::vector<nlohmann::json> &&entities)
-      : prefabName(name), rootEntityGUID(root), entities{entities} {}
+	PrefabAsset(const std::string& name, const Assets::GUID& root,
+							std::vector<nlohmann::json> &&entities)
+			: prefabName(name), rootEntityGUID(root), entities{entities} {}
 };
 } // namespace Prefab
 } // namespace PAIN
