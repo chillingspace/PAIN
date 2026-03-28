43bc444345d1a5dec6ba88aeaf4fa4cbf97456de
bsohzj <144084859+bsohzj@users.noreply.github.com>
2026-03-28 23:03:26 +0800

    Update editor for UIText
    Added Reset Button to Controls settings too

--- PAINEngine/src/PAINEngine/LayeredSystems/LevelEditor/Panels/ComponentsPanel.cpp
+++ PAINEngine/src/PAINEngine/LayeredSystems/LevelEditor/Panels/ComponentsPanel.cpp
@@ -762,8 +762,16 @@ namespace PAIN {
 					}
 
 					// Font position
-					ImGui::Text("Text Position: (%.8f, %.8f)", text.text_pos.x,
-								text.text_pos.y);
+					//ImGui::Text("Text Position: (%.8f, %.8f)", text.text_pos.x,
+					//			text.text_pos.y);
+					changed |= ImGui::DragFloat2(
+						"Text Position",
+						&text.text_pos.x,
+						1.0f,
+						-10000.0f,
+						10000.0f,
+						"%.2f"
+					);
 
 					// Font size
 					changed |= ImGui::DragFloat("Font Size", &text.font_size, 0.2f, 6.0f,
