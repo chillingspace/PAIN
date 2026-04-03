87ac9000a919c7cb84fbd70c51fb75068bb877dd
MintKK <30825268+MintKK@users.noreply.github.com>
2026-04-03 11:49:24 +0800

    Remove incorrect Android keyboard bindings

--- PAINEngine/src/PAINEngine/Systems/Scripting/GameScriptingSystem.cpp
+++ PAINEngine/src/PAINEngine/Systems/Scripting/GameScriptingSystem.cpp
@@ -278,53 +278,8 @@ namespace PAIN {
 #endif
 
 #ifdef PN_PLATFORM_ANDROID
-                //  raw key codes no glfw for android
-            case 32: return "SPACE";
-            case 257: return "ENTER";
-
-            case 65: return "A";
-            case 66: return "B";
-            case 67: return "C";
-            case 68: return "D";
-            case 69: return "E";
-            case 70: return "F";
-            case 71: return "G";
-            case 72: return "H";
-            case 73: return "I";
-            case 74: return "J";
-            case 75: return "K";
-            case 76: return "L";
-            case 77: return "M";
-            case 78: return "N";
-            case 79: return "O";
-            case 80: return "P";
-            case 81: return "Q";
-            case 82: return "R";
-            case 83: return "S";
-            case 84: return "T";
-            case 85: return "U";
-            case 86: return "V";
-            case 87: return "W";
-            case 88: return "X";
-            case 89: return "Y";
-            case 90: return "Z";
-
-            case 48: return "0";
-            case 49: return "1";
-            case 50: return "2";
-            case 51: return "3";
-            case 52: return "4";
-            case 53: return "5";
-            case 54: return "6";
-            case 55: return "7";
-            case 56: return "8";
-            case 57: return "9";
-
-            case 262: return "KEY_R";
-            case 263: return "KEY_L";
-            case 264: return "KEY_D";
-            case 265: return "KEY_U";
-
+            // TODO: Implement native AKEYCODE_* mappings if hardware keyboard support is needed
+            return std::nullopt;
 #endif
 
             default: return std::to_string(keyCode);
